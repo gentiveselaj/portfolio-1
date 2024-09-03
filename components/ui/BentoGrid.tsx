8663d65503f2ecc1ba95fedf45dce15ef5d642f3
gentiveselaj <gentzveselaj@gmail.comm>
2024-09-04 01:04:13 +0200

    first commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["Javascript", "Php", "Laravel"];
+  const rightLists = ["Ionic", "Angular", "mySQL"];
 
   const [copied, setCopied] = useState(false);
 
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "gentzveselaj@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
