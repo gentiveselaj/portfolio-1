d942c65d81aed6fc69a022721168de8c5f19258d
gentiveselaj <127799210+gentiveselaj@users.noreply.github.com>
2024-09-04 11:29:46 +0200

    Update page.jsx

--- app/sentry-example-page/page.jsx
+++ app/sentry-example-page/page.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import Head from "next/head";
@@ -78,28 +77,3 @@ export default function Page() {
     </div>
   );
 }
-=======
-
-
-
-"use client";
-
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
-import { useEffect } from "react";
-
-export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
-  return (
-    <html>
-      <body>
-        <Error />
-      </body>
-    </html>
-  );
-}
-
-
